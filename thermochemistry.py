d108a900448c3edcef9010c406bbcec5553a0b7f
Unknown <ty13946@my.bristol.ac.uk>
2018-11-05 12:51:52 +0000

    Updates thermochemistry

--- thermochemistry.py
+++ thermochemistry.py
@@ -1,82 +1,82 @@
-import readline, glob, os
+#!/usr/bin/env python3
+import argparse
+import os
 
 
-def complete(text, state):
-    return (glob.glob(text+'*')+[None])[state]
+def get_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", action='store', help='.out file(s) to extract thermochemistry from', nargs='+')
+
+    return parser.parse_args()
 
 
-readline.set_completer_delims(' \t\n;')
-readline.parse_and_bind("tab: complete")
-readline.set_completer(complete)
+def write_thermochem(filenames):
 
-directory = input("Folder from which to extract themochemistry  ")
+    extract_file = open('thermochem.csv', 'w')
+    print('Species,' + 'E,' + 'G,' + 'H' + '\n', file=extract_file)
 
-if directory.endswith('/'):
-    pass
-else:
-    directory = directory + '/'
+    for out_file in filenames:
 
+        name = out_file.replace('.out', '')
 
-with open(directory + 'thermochem.csv', 'w') as extract_file:
-    extract_file.write('Species,' + 'E,' + 'G,' + 'H' + '\n')
+        print(name + ',', file=extract_file)
 
+        '''
+        -----------------------------Parse the output file--------------------------
+        '''
 
-for out_file in glob.glob(directory + "*.out"):
+        vib_freq_section = False
+        opt_done = False
+        opt_ts = False
+        opt = False
+        freq = False
+        E = 0
+        H = 0
+        G = 0
 
-    extract_file = open(directory + 'thermochem.csv', 'a')
-    extract_file.write(os.path.basename(out_file).replace('.out', '') + ',')
+        with open(out_file, 'r') as out_file_r:
 
-    '''
-    -----------------------------Pass the output file--------------------------
-    '''
+            for line in out_file_r:
+                # Grab the keywords from the output file
+                if '1> !' in line:
+                    for item in line.split():
+                        if item == 'Opt':
+                            opt = True
+                        if item == 'OptTS':
+                            opt_ts = True
+                        if item == 'Freq':
+                            freq = True
 
-    vib_freq_section = False
-    opt_done = False
-    imag_freq = None
-    OptTS = False
-    Opt = False
-    Freq = False
-    E = 0
-    H = 0
-    G = 0
+                if freq:
+                    # If a frequency calculation has been requested then output themochemistry
 
-    with open(out_file, 'r') as out_file_r:
+                    if '*** OPTIMIZATION RUN DONE ***' in line:
+                        opt_done = True
 
-        for line in out_file_r:
-            #Grab the keywords from the output file
-            if '1> !' in line:
-                for item in line.split():
-                    if item == 'Opt':
-                        Opt = True
-                    if item == 'OptTS':
-                        OptTS = True
-                    if item == 'Freq':
-                        Freq = True
+                    if opt_done:
+                        if line.startswith("VIBRATIONAL FREQUENCIES"):
+                            vib_freq_section = True
 
-            if Freq == True:
-                #If a frequnecy calculation has been requested then output themochemistry
+                    if vib_freq_section:
+                            if '***imaginary mode***' in line and opt:
+                                print('imaginary freq in', out_file, 'species is not a true minimum')
 
-                if '*** OPTIMIZATION RUN DONE ***' in line:
-                    opt_done = True
+                            if '***imaginary mode***' in line and opt_ts:
+                                imag_freq = line.split()[1]
+                                print(name, ' imaginary frequnecy is  ', imag_freq)
 
-                if opt_done:
-                    if line.startswith("VIBRATIONAL FREQUENCIES"):
-                        vib_freq_section = True
+                    if opt_done == True and 'Electronic energy' in line:
+                        E = float(line.split()[-2])
+                    if opt_done == True and 'Total Enthalpy' in line:
+                        H = float(line.split()[-2]) - E
+                    if opt_done == True and 'Final Gibbs free enthalpy' in line:
+                        G = float(line.split()[-2]) - E
 
-                if vib_freq_section:
-                        if '***imaginary mode***' in line and Opt == True:
-                            print('imaginary freq in', out_file, 'species is not a true minimum')
-
-                        if '***imaginary mode***' in line and OptTS == True:
-                            imag_freq = line.split()[1]
-                            print(os.path.basename(out_file).replace('.out', ''), ' imaginary frequnecy is  ', imag_freq)
-
-                if opt_done == True and 'Electronic energy' in line:
-                    E = float(line.split()[-2])
-                if opt_done == True and 'Total Enthalpy' in line:
-                    H = float(line.split()[-2]) - E
-                if opt_done == True and 'Final Gibbs free enthalpy' in line:
-                    G = float(line.split()[-2]) - E
+        print(str(E) + ',' + str(G) + ',' + str(H) + '\n', file=extract_file)
 
 
-    extract_file.write(str(E) + ',' + str(G) + ',' + str(H) + '\n')
+if __name__ == '__main__':
+
+    args = get_args()
+    write_thermochem(args.filenames)
