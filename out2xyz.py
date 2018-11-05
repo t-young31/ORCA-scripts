1d4c11b5dbdf515dc87f7038a7dda3ae1cfe7caf
Unknown <ty13946@my.bristol.ac.uk>
2018-11-05 12:12:12 +0000

    Added out2xyz

---
+++ out2xyz.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import argparse
+
+
+def get_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", action='store', help='.out file to convert to a .xyz. Does not need to be finished')
+
+    return parser.parse_args()
+
+
+def get_xyzs(filename):
+
+    out_file_lines = [line for line in open(filename, 'r')]
+    end_block, start_block = False, False
+    xyz_lines = []
+
+    for line in reversed(out_file_lines):
+
+        if 'CARTESIAN COORDINATES (ANGSTROEM)' in line:
+            start_block = True
+
+        if end_block and not start_block:
+            if len(line.split()) == 4:
+                xyz_lines.append(line.split())
+
+        if 'CARTESIAN COORDINATES (A.U.)' in line:
+            end_block = True
+
+    return xyz_lines
+
+
+def xyzs_to_xyz_file(xyzs, filename):
+
+    with open(filename, 'w') as xyz_file:
+        print(len(xyzs), '\n', filename, sep='', file=xyz_file)
+        [print(*line, sep='\t', file=xyz_file) for line in xyzs]
+
+
+if __name__ == '__main__':
+
+    args = get_args()
+    xyz_list = get_xyzs(args.filename)
+    xyzs_to_xyz_file(xyz_list, filename=args.filename.replace('.out', '.xyz'))
